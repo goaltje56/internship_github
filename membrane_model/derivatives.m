7280349834f30e42c8b54f68cb700b722ff16f7a
goaltje56 <gotoos@hotmail.com>
2018-11-02 18:18:07 +0100

    removed membrane folder

--- membrane_model/derivatives.m
+++
@@ -1,4 +0,0 @@
-function [dudx ] = derivatives(NPI,u, x, x_u)
-    for I = 2:NPI+1
-        dudx(I) = (u(i+1)-u(i))/(x_u(i+1)-x_u(i));
-end
--- membrane_model/fric_u.m
+++
@@ -1,8 +0,0 @@
-function F_u = fric_u(NPI, mu, x, x_u, u)
-
-    for I=2:NPI+2 % start at 3 beceause I-2 is outside domain for i=1
-        i = I;
-        F_u(i) = -((mu(I-1)*(x(I)-x_u(i)))+(mu(I)*(x_u(i)-x(I-1))))*u(i)/ (x(I)-x(I-1)); 
-    end
-    
-end
